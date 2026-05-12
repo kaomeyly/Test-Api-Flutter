afb860d83539992b28f14dfe774853109f487e6c
Kao Meyly <kaomeyly@gmail.com>
2026-05-12 21:33:04 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:dio_todo_list/routes/app_pages.dart';
 import 'package:dio_todo_list/routes/app_routes.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:get/route_manager.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() {
   GetStorage.init();
+
   runApp(const MainApp());
 }
 
@@ -14,10 +15,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var box = GetStorage();
-    box.read("token");
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.login,
       getPages: AppPages.getPages,
     );
